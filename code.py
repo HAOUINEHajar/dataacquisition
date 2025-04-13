34c0a6bf0b807101556a696d6f2a72011629290a
HAOUINE Hajar <141008552+HAOUINEHajar@users.noreply.github.com>
2025-04-13 11:11:25 +0100

    Update code.py

--- code.py
+++ code.py
@@ -3,11 +3,7 @@ import pandas as pd
 
 def show_sheets_page():
     st.title("Lecture de Google Sheets dans Streamlit")
-
-    # Remplace cet ID par celui de ta feuille Google Sheets
-    sheet_id = "1KgQ_MTxfQeEqkJi1brg-5hOdqhsshJ9arzCRJcOtvns"
-    sheet_name = "ensam-01"
-    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    url = "https://docs.google.com/spreadsheets/d/1KgQ_MTxfQeEqkJi1brg-5hOdqhsshJ9arzCRJcOtvns/edit?gid=0#gid=0"
 
     try:
         df = pd.read_csv(url)
